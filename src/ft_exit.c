7674c13c0201a7d9bc9377cdee78d26447224d2a
Arthur Beaufils <abeaufil@c2r1p6.42lehavre.fr>
2025-05-29 16:02:06 +0200

    Fix Norm ft_exit.c

--- src/ft_exit.c
+++ src/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: garside <garside@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abeaufil <abeaufil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 19:11:06 by garside           #+#    #+#             */
-/*   Updated: 2025/05/21 17:58:32 by garside          ###   ########.fr       */
+/*   Updated: 2025/05/29 16:01:17 by abeaufil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,21 @@ int	ft_isalldigit(char *str)
 	return (1);
 }
 
+void	close_fd(t_data *data, int stdin, int stdout)
+{
+	close(stdin);
+	close(stdout);
+	free_cmd_list(data);
+	free_data(data);
+}
+
 int	ft_exit(t_data *data, t_cmd *cmd, int stdin, int stdout)
 {
 	int	code;
 
 	if (!cmd->args[1])
 	{
-		close(stdin);
-		close (stdout);
-		free_cmd_list(data);
-		free_data(data);
+		close_fd(data, stdin, stdout);
 		ft_printf("exit\n");
 		exit(0);
 	}
@@ -44,19 +49,13 @@ int	ft_exit(t_data *data, t_cmd *cmd, int stdin, int stdout)
 		ft_putstr_fd("exit: ", 1);
 		ft_putstr_fd(cmd->args[1], 1);
 		ft_putstr_fd(": numeric argument required\n", 1);
-		close(stdin);
-		close (stdout);
-		free_cmd_list(data);
-		free_data(data);
+		close_fd(data, stdin, stdout);
 		exit(2);
 	}
 	else if (!cmd->args[1])
 		return (ft_putstr_fd("exit: too many arguments\n", 2), 1);
 	code = ft_atoi(cmd->args[1]);
-	close(stdin);
-	close (stdout);
-	free_cmd_list(data);
-	free_data(data);
+	close_fd(data, stdin, stdout);
 	ft_printf("exit\n");
 	exit (code);
 	return (0);
